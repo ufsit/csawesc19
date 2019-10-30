f94435155649e63e57fa03b27d57456d97ced519
Grant Hernandez <grant.h.hernandez@gmail.com>
2019-10-30 16:47:52 -0400

    Faster monitor

--- final/monitor.py
+++ final/monitor.py
@@ -3,7 +3,20 @@ import hashlib
 import sys
 import glob
 
-ser = serial.Serial(glob.glob('/dev/tty.usbmodem*')[0], 2000000)
-
 while True:
-    sys.stdout.write(ser.read(1024))
+    print("--------- POLLING -----------")
+    devices = glob.glob('/dev/tty.usbmodem*')
+    if len(devices) == 0:
+        continue
+
+    device = devices[0]
+
+    try:
+        ser = serial.Serial(device, 2000000)
+
+        print("--------- CONNECTED TO %s-----------" % device)
+        while True:
+            sys.stderr.write(ser.read(1024))
+
+    except serial.serialutil.SerialException:
+        print("--------- EXCEPTION -----------")
